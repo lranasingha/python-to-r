b3eb8b4a98515cb4e0ea6b719779f4de27dbb778
9109495 <a9109495@9109495s-MacBook-Pro.local>
2017-10-16 15:42:42 +0100

    adding a docker-compose file for cloudant

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(name="python-to-r"
-      version="BUILD"
-      description="A simple wrapper app which talks to R functions using rpy2"
-      packages=['python-to-r']
-      )
-    
