b739c431d5b6f34ddff3627d98642e4b5952b077
Laksitha <lakshitha.hav@gmail.com>
2017-10-25 13:28:05 +0100

    added env vars to get db2 connection properties

--- db/db2_interface.py
+++ db/db2_interface.py
@@ -3,6 +3,7 @@ import ibm_db_dbi
 import os
 import pandas as pd
 import numpy as np
+import os
 
 __db2_connection__ = None
 
@@ -11,16 +12,35 @@ def connect_db2(db_name):
     if is_bluemix_env:
         print("no bluemix db2 support yet!!")
     else:
-        db_con_string = ("DATABASE={db};"
-                          "HOSTNAME={hostname};"
-                          "PORT={port};"
-                          "PROTOCOL=TCPIP;"
-                          "UID={username};"
-                          "PWD={password};").format(db=db_name, hostname="localhost", username="db2inst1", password="db2-dev-pw", port=50000)
+        db_con_string = (
+                              "DATABASE={db};"
+                              "HOSTNAME={hostname};"
+                              "PORT={port};"
+                              "PROTOCOL=TCPIP;"
+                              "UID={username};"
+                              "PWD={password};").format(
+                                  db=os.getenv("db2_dbname", db_name),
+                                  hostname=os.getenv("db2_host", "localhost"),
+                                  username=os.getenv("db2_user", "db2inst1"),
+                                  password=os.getenv("db2_password", "db2-dev-pw"),
+                                  port=os.getenv("db2_port", 50000)
+                              )
+
+        ssl_enabled = os.getenv("db2_ssl_enabled", False)
+
+        if ssl_enabled :
+            db_con_string = (
+            db_con_string +
+            "SECURITY=SSL;"
+            "SSLSERVERCERTIFICATE={certpath};".format(certpath=os.getenv("db2_ssl_cert", ""))
+            )
+
+        print(db_con_string)
 
         global __db2_connection__
         __db2_connection__ = ibm_db.connect(db_con_string, '', '')
         print("connected to local db2 instance!!")
+
 
 def insert(sql, params):
     if __db2_connection__ is not None:
@@ -35,7 +55,7 @@ def get_all_events():
     if __db2_connection__ is not None:
         dbi_conn = ibm_db_dbi.Connection(__db2_connection__)
 
-        events_dataframe = pd.read_sql("SELECT id,description,status FROM eventsdb.events LIMIT(800000)", dbi_conn)
+        events_dataframe = pd.read_sql("SELECT id,description,status FROM eventsdb.events", dbi_conn)
         return (len(events_dataframe), events_dataframe.memory_usage(index=True).sum())
 
 def close_db2():
